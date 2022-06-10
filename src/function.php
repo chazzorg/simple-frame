5796875cb0f17caa9159bcc76e8ac5875fa67557
zz <zz@admin.com>
2022-06-10 09:31:16 +0800

    函数补充

--- src/function.php
+++ src/function.php
@@ -165,3 +165,20 @@ if (function_exists('convertname') === false) {
         return $ucfirst ? ucfirst($str) : $str;
     }
 }
+
+/**
+ * 系统函数补充
+ * @return array headers 参数
+ */
+if (!function_exists('getallheaders')) {
+    function getallheaders()
+    {
+        $headers = array();
+        foreach ($_SERVER as $name => $value) {
+            if (substr($name, 0, 5) == 'HTTP_') {
+                $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+            }
+        }
+        return $headers;
+    }
+}
